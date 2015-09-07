f09f7bdefdd3c69d2626fd8fd551f44de97226bc
MarlonCastillo <marlonjcastillo@hotmail.com>
2015-09-07 14:29:56 -0600

    Update ArbolBusqueda.java

--- Hoja7/src/hoja7/ArbolBusqueda.java
+++ Hoja7/src/hoja7/ArbolBusqueda.java
@@ -9,9 +9,14 @@ Sección 30
 Hoja de trabajo 7
 -------------------------------------------------------------------*/
 
+//este codigo fue tomado del siguiente enlace:
+//http://www.lawebdelprogramador.com/codigo/Java/2257-Arboles-binarios-de-busqueda.html
+
+//clase que permite implementar un arbol binario de busqueda
 import java.util.*;
 public class ArbolBusqueda {
- 
+
+//clase para crear los nodos del arbol 
     private class nodoArbol {
         private ArbolBusqueda hd;
         private ArbolBusqueda hi;
@@ -24,16 +29,17 @@ public class ArbolBusqueda {
         }
     }
     public nodoArbol raiz;
- 
+//creamos la raiz del arbol
     public void ArbolBusqueda(){
         nodoArbol raiz = new nodoArbol();
     }
- 
+ //verificamos si el arbol esta vacio o no
     public boolean esVacio(){
         return (raiz == null);
     }
- 
+//insertamos un nuevo elemento en el arbol
     public void insertar(String a){
+        //si el arbol esta vacio entonces el elemento ingresado se convierte en la raiz
         if (esVacio()) {
             nodoArbol nuevo = new nodoArbol();
             nuevo.dato = a;
@@ -42,6 +48,8 @@ public class ArbolBusqueda {
             raiz = nuevo;
         }
         else {
+            //si no esta vacio el arbol se verifica si se coloca al lado derecho o izquierdo segun sea mayor o menor respectivamente
+            //compareTo regresa un 0 si son iguales, un positivo si el primero es mayor al segundo y un negativo si el primero es menor al segundo
             int b = a.compareTo(raiz.dato);
             if (b>0) {
                 (raiz.hd).insertar(a);
@@ -51,7 +59,8 @@ public class ArbolBusqueda {
             }
         }
     }
- 
+
+//permite hacer un recorrido preOrder (raiz, izquierdo, derecho)
     public void preOrder(){
         if (!esVacio()) {
             System.out.print( raiz.dato + ", "  );
@@ -59,7 +68,8 @@ public class ArbolBusqueda {
             raiz.hd.preOrder();
         }
     }
- 
+
+//permite hacer un recorrido preOrder (izquierdo, raiz, derecho)
     public void inOrder(){
         if (!esVacio()) {
             raiz.hi.inOrder();
@@ -67,7 +77,8 @@ public class ArbolBusqueda {
             raiz.hd.inOrder();
         }
     }
- 
+
+//permite hacer un recorrido preOrder (izquierdo, derecho, raiz)
     public void posOrder(){
         if (!esVacio()) {
             raiz.hd.posOrder();
@@ -76,16 +87,20 @@ public class ArbolBusqueda {
  
         }
     }
- 
+
+//busca un elemento especifico en el arbol
     public ArbolBusqueda buscar(String a){
         ArbolBusqueda arbolito = null;
         int b = a.compareTo(raiz.dato);
+        //si el arbol no esta vacio comienza la busqueda, de lo contrario retorna null
         if (!esVacio()) {
+            //si el valor que se desea buscar es igual a la raiz
             if (b==0) {
             System.out.println(a);
             return this;
             }
             else {
+                //si no es igual a la raiz verifica si es mayor o menor a la misma, cambia la raiz y vuelve a llamar de nuevo al metodo
                 if (b<0) {
                     arbolito = raiz.hi.buscar(a);
                 }
